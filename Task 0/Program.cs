699b97493927f85a44b05fb4d9e65592ce9d4289
Vladislav <SimashVlad@yandex.ru>
2022-04-25 22:56:25 +0500

    Первая программа

---
+++ Task 0/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрвт (Число умноженное на само себя)
+
+Console.Clear();
+Console.Write("Введите целое число -> ");
+int num = int.Parse(Console.ReadLine());
+int numResult = num * num;
+Console.Write("Квадрат числа равен -> ");
+Console.WriteLine(numResult);
+Console.ReadKey();
