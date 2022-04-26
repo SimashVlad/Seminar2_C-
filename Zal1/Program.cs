1484fef74491b546bcac2b584d01418901508f03
Vladislav <SimashVlad@yandex.ru>
2022-04-26 16:25:24 +0500

    1 Задание в сессионном зале как проще

--- Zal1/Program.cs
+++ Zal1/Program.cs
@@ -2,15 +2,5 @@
 int num1 = int.Parse(Console.ReadLine());
 Console.Write("Введите число b ");
 int num2 = int.Parse(Console.ReadLine());
-//Console.Write("Квадрат числа равен -> ");
-int numResult = num1 * num1;
-if (numResult == num2)
-{
-    Console.Write("Да");
-}
-else
-{
-    Console.Write("Нет");
-}
-//Console.WriteLine(numResult);
-Console.ReadKey();
+bool result = num2 * num2 == num1;
+Console.WriteLine(result ? "Да" : "Нет");
---
+++ Zal2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите номер дня недели ");
+int num1 = int.Parse(Console.ReadLine());
+if(num1 == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+Console.ReadKey();
