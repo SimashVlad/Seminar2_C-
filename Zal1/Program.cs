b33669b9e5dc6ef27a28d0a1eb24e96915f1bc4a
Vladislav <SimashVlad@yandex.ru>
2022-04-26 15:34:05 +0500

    1 Задание в сессионном зале

---
+++ Zal1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число a ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число b ");
+int num2 = int.Parse(Console.ReadLine());
+//Console.Write("Квадрат числа равен -> ");
+int numResult = num1 * num1;
+if (numResult == num2)
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}
+//Console.WriteLine(numResult);
+Console.ReadKey();
