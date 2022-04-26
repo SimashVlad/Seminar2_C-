d7e77a64bf354041fa4c4009a288633b7522386d
Vladislav <SimashVlad@yandex.ru>
2022-04-26 17:08:59 +0500

    Задача с нахождением числа N от -N до +N

---
+++ Zal3/Program.cs
@@ -0,0 +1,15 @@
+﻿//int num = int.Parse(Console.ReadLine());
+//Console.WriteLine(num % 10);
+
+Console.WriteLine("Введите целое число");
+int N = int.Parse(Console.ReadLine());
+int Y = -N;
+while (Y <=N)
+{
+    Console.Write(Y +" ");
+    Y++;
+}
+
+
+
+
