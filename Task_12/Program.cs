10b2090c9975ece6d45f3728586bd6624dc02243
Vladislav <SimashVlad@yandex.ru>
2022-05-15 00:29:08 +0500

    Task_12_Исправление

--- Task_12/Program.cs
+++ Task_12/Program.cs
@@ -1,10 +1,12 @@
-﻿
-int number1 = new Random().Next(10, 100);
-int number2 = new Random().Next(10, 100);
-Console.WriteLine($"первое число = {number1}");
-Console.WriteLine($"второе число = {number2}");
+﻿/*Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число
+кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+34, 5 -> не кратно, остаток 4 16, 4 -> кратно  */
+Console.Write("Введите первое число :");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число :");
+int number2 = int.Parse(Console.ReadLine());
 int result = number1 % number2;
-if(number1 > number2)
+if (number1 > number2)
 {
     if (result == 0)
     {
@@ -15,7 +17,7 @@ if(number1 > number2)
         Console.WriteLine($"Число не кратно, остаток = {result}");
     }
 }
-else 
+else
 {
     Console.WriteLine("Число не делится");
 }
