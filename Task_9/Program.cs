9d3bd6b0ee23c9760ee1f321bd4703fd80237abd
Vladislav <SimashVlad@yandex.ru>
2022-05-14 22:33:39 +0500

    Task_9

---
+++ Task_9/Program.cs
@@ -0,0 +1,9 @@
+﻿
+int number = new Random().Next(10, 100);
+int num1 = number / 10;
+int num2 = number % 10;
+Console.WriteLine(number);
+Console.WriteLine($"первое число = {num1}");
+Console.WriteLine($"второе число = {num2}");
+Console.Write("максимальное число = ");
+Console.WriteLine(num1 > num2 ? num1 : num2); // Краткая запись If Else
