ce03535482448d9492b73e5d027318eecdfc7051
Vladislav <SimashVlad@yandex.ru>
2022-05-17 21:05:03 +0500

    Task_17

--- Task_17/Program.cs
+++ Task_17/Program.cs
@@ -3,19 +3,26 @@ Console.WriteLine("Введите координату х: ");
 int x = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите координату y: ");
 int y = int.Parse(Console.ReadLine());
-if (x > 0 && y > 0)
+if (x < 0 || y < 0 || x > 0 || y > 0)
 {
-    Console.WriteLine("Диапазон находится в 1 четверти");
+    if (x > 0 && y > 0)
+    {
+        Console.WriteLine("Диапазон находится в 1 четверти");
+    }
+    else if (x < 0 && y > 0)
+    {
+        Console.WriteLine("Диапазон находится во 2 четверти");
+    }
+    else if (x < 0 && y < 0)
+    {
+        Console.WriteLine("Диапазон находится в 3 четверти");
+    }
+    else if (x > 0 && y < 0)
+    {
+        Console.WriteLine("Диапазон находится в 4 четверти");
+    }
 }
-else if (x < 0 && y > 0)
+else
 {
-    Console.WriteLine("Диапазон находится во 2 четверти");
-}
-else if (x < 0 && y < 0)
-{
-    Console.WriteLine("Диапазон находится в 3 четверти");
-}
-else if (x > 0 && y < 0)
-{
-    Console.WriteLine("Диапазон находится в 4 четверти");
+    Console.WriteLine("Некоректное значение");
 }
