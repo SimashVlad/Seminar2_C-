d694971cb62cf91e9a5ca8b926d963896cd8b55a
Vladislav <SimashVlad@yandex.ru>
2022-05-17 20:56:19 +0500

    Task_15Array

---
+++ Task_15Array/Program.cs
@@ -0,0 +1,9 @@
+﻿// 
+Console.WriteLine("Введите число дня недели от 1 до 7: ");
+var day = Convert.ToInt32(Console.ReadLine());
+string[] isWeekend = {"нет", "нет", "нет", "нет", "нет", "нет", "да", "да"};
+if(day > 0 && day < 8)
+{
+    Console.WriteLine(isWeekend[day -1]);
+}
+else Console.WriteLine("Вы ввели некорректное значение!");
