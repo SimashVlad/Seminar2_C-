6485201068a8b5a39bc5399ae34b48cc913ca080
Vladislav <SimashVlad@yandex.ru>
2022-05-17 22:37:16 +0500

    Task18+21

---
+++ Task_18/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите номер четверти: ");
+int num = int.Parse(Console.ReadLine());
+if (num == 1)
+{
+    Console.WriteLine("Диапазон находится в x > 0, y > 0");
+}
+else if (num == 2)
+{
+    Console.WriteLine("Диапазон находится в x < 0, y > 0");
+}
+else if (num == 3)
+{
+    Console.WriteLine("Диапазон находится в x < 0, y < 0");
+}
+else if (num == 4)
+{
+    Console.WriteLine("Диапазон находится в x > 0, y < 0");
+}
+else
+{
+    Console.WriteLine("Некоректное значение, введите число от 1 до 4.");
+}
---
+++ Task_21/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите координаты точки A: ");
+int numA1 = int.Parse(Console.ReadLine());
+int numA2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B: ");
+int numB1 = int.Parse(Console.ReadLine());
+int numB2 = int.Parse(Console.ReadLine());
+
+int number1 = numA1 - numB1;
+int number2 = numA2 - numB2;
+
+double result = Math.Sqrt(number1 * number1 + number2 * number2);
+Console.WriteLine($"Расстояние между точками =  {result}");
