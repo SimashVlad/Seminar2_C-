b9f9a6087ed211e1031d58402399e7e735e65fca
Vladislav <SimashVlad@yandex.ru>
2022-05-22 23:46:07 +0500

    Task_24

--- Task_24/Program.cs
+++ Task_24/Program.cs
@@ -10,10 +10,10 @@
 Console.Write("Введите натуральное число ");
 int num = int.Parse(Console.ReadLine());
 
-int GetSumNumbers(int number)
+int GetSumNumbers(int number)                               // метод int  который что то возвращает
 {
     int sum = 0;
-    for (int i = 1; i <= num; i++)
+    for (int i = 1; i <= number; i++)
     {
         sum = sum + i;
     }
@@ -22,3 +22,22 @@ int GetSumNumbers(int number)
 
 int sumResult = GetSumNumbers (num);
 Console.WriteLine($"Сумма чисел от 1 до {num} = {sumResult}");
+
+
+// не корректный метод который не рекомендуется использовать
+/* Console.Write("Введите натуральное число ");
+int num = int.Parse(Console.ReadLine());
+
+void GetSumNumbers(int number)                              // метод void  который не возвращает ничего
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        sum = sum + i;
+    }
+    Console.WriteLine($"Сумма чисел от 1 до {num} = {sum}");
+}
+
+GetSumNumbers(num); */
+
+
